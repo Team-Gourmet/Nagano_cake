d829b2a18ec8d635c4a8cb549c7d0d9a97d1a575
EC2 Default User <ec2-user@ip-172-31-23-203.ap-northeast-1.compute.internal>
2023-06-19 08:00:58 +0000

    Addlogin

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -7,4 +7,16 @@ class Public::CustomersController < ApplicationController
 
   def quit
   end
+  
+  def update
+  end
+  
+  def withdraw
+  end
+  
+  
+  private
+　def customer_params
+　  params_require(:customer).parmit(:last_name, :first_name,:last_name_kana, :first_name_kana,:email,:postcode,:address,:phone_number, :password,:password_confirmation)
+　end
 end
