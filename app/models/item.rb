27180f7bffbcfc78fa2fe86492139a7ee6653a2b
EC2 Default User <ec2-user@ip-172-31-14-164.ap-northeast-1.compute.internal>
2023-06-20 07:39:20 +0000

    Add Admin/Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,18 @@ class Item < ApplicationRecord
   has_many :carts, dependent: :destroy
   has_many :order_details
   has_one_attached :image
-  
-  
-  
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
+  end
+
   def with_tax_price
    (price * 1.1).floor
   end
-  
-  
+
+
 end
