aa2155f27d33e232ac12f976e694457fb9009214
takuitose <132122425+takuitose@users.noreply.github.com>
2023-06-20 08:42:20 +0000

    Add Customer(EC)

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,17 +1,27 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def quit
+    @customer = current_customer
   end
   
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to costomers_my_page_path(@customer)
   end
   
   def withdraw
+    @customer = current_customer
+    @customer.update(is_withdraw :true)
+    reset_session
+    redirect_to root_path
   end
   
   
--- db/schema.rb
+++ db/schema.rb
@@ -63,14 +63,6 @@ ActiveRecord::Schema.define(version: 2023_06_18_083935) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "postcode", null: false
-    t.text "address", null: false
-    t.string "phone_number", null: false
-    t.boolean "is_withdrawal", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
