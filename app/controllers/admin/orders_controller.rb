5c55176460251a06821e82bb24bdadef16de3bfe
EC2 Default User <ec2-user@ip-172-31-23-203.ap-northeast-1.compute.internal>
2023-06-26 09:56:46 +0000

    AddAdminOrder2

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,11 +9,12 @@ class Admin::OrdersController < ApplicationController
     @order.update(order_params)
     if @order.status == "confirmed"
       @order.order_details.each do |order_detail|
-        order_detail.production_status = OrderDetail.production_status.key(1)
+        order_detail.production_status = OrderDetail.production_statuses.key(1)
         order_detail.save
+       end
     end
       redirect_to admin_order_path(@order.id)
-    end
+    
   end
 
   private
