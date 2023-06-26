6c3041a3e82926215bececc0670faa15c0beaf77
takuitose <132122425+takuitose@users.noreply.github.com>
2023-06-26 07:24:01 +0000

    add admin/order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,24 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @ordre = Order.find(params[:id])
+    @total = 0
+  end
+  
+  def update
+    @ordre = Order.find(params[:id])
+    @order.update(order_params)
+    if @order.status == "confirmed"
+      @order.order_details.each do |order_detail|
+        order_detail.production_status = OrderDetail.production_status.key(1)
+        order_detail.save
+    end
+      redirect_to admin_order_path(@order)
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
--- app/controllers/admin/orders_details_controller.rb
+++ app/controllers/admin/orders_details_controller.rb
@@ -1,2 +1,32 @@
 class Admin::OrdersDetailsController < ApplicationController
+    def update
+      @order_detail = OrderDetail.find(params[:id])
+      @order = OrderDetail.order
+      @order_details = @order.order_details
+  
+      is_updated = true
+        @order_detail.update(order_detail_params)
+        @order.update(status: 2) if @order_detail.production_status_i18n == "manufacturing"
+        # ②製作ステータスが「製作中」のときに、注文ステータスを「製作中」に更新する。
+        # ここから下の内容は③の内容になります。
+        # 紐付いている注文商品の製作ステータスが "すべて" [製作完了]になった際に注文ステータスを「発送準備中」に更新させたいので、
+        @order_details.each do |order_detail| #　紐付いている注文商品の製作ステータスを一つ一つeach文で確認していきます。
+        
+        is_updated = false if order_detail.production_status_i18n != "finish" # 製作ステータスが「製作完了」ではない場合 
+        
+        end
+        
+        @order.update(status: 3) if is_updated
+        # is_updatedがtrueの場合に、注文ステータスが「発送準備中」に更新されます。上記のif文でis_updatedがfalseになっている場合、更新されません。
+     
+        redirect_to admin_order_path(@order.id)
+        
+    end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:production_status)
+  end
+
 end
